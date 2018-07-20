931cfe7a6305d383ce60f60bd1f5790bb668ace1
Masayuki Imaizumi <masayuki.imaizumi@accenture.com>
2018-07-20 19:45:01 +0900

    ItemCode, 画像URL, Actor取得を実装

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-//	url := "http://www.dmm.com/digital/idol/-/detail/=/cid=5013tsds42319/"
+	url := "http://www.dmm.com/digital/idol/-/detail/=/cid=5013tsds42319/"
 
-	result := NewHoge()
+	result := New(url)
 
+	fmt.Println(result.ItemCode)
 	fmt.Println(result.Title)
+	fmt.Println(result.PackageImageThumbURL)
+	fmt.Println(result.PackageImageURL)
+	for index, value := range result.ActressList {
+		fmt.Println(strconv.Itoa(index) + ":" + value.Name)
+	}
 }
--- type.go
+++ type.go
@@ -1,26 +1,67 @@
 package main
 
 import (
+	"regexp"
 	"github.com/PuerkitoBio/goquery"
 )
 
 type ItemOfDmmComIdol struct {
-	ItemCode string
-	Title string
+	ItemCode             string
+	Title                string
+	PackageImageThumbURL string
+	PackageImageURL      string
+	ActressList          []*Actress
+}
+
+type Actress struct {
+	Id string
+	Name string
 }
 
 
-func NewHoge() *ItemOfDmmComIdol {
-	url := "http://www.dmm.com/digital/idol/-/detail/=/cid=5013tsds42319/"
+func New(url string) *ItemOfDmmComIdol {
 
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
 		panic(err)
 	}
 
+
 	result := ItemOfDmmComIdol{}
+
+	cidMatcher := regexp.MustCompile(`cid=([^/]+)`)
+	itemCode := cidMatcher.FindString(url)
+	itemCode = cidMatcher.ReplaceAllString(itemCode, "$1")
+	result.ItemCode = itemCode
+
 	selection := doc.Find("#title")
 	result.Title = selection.First().Text()
+
+	doc.Find("#package-src-"+itemCode).Each(func(index int, selection *goquery.Selection) {
+		img_src, exists := selection.Attr("src")
+		if(exists) {
+			result.PackageImageThumbURL = img_src
+		}
+	})
+
+	doc.Find("#"+itemCode).Each(func(index int, selection *goquery.Selection) {
+		a_href, exists := selection.Attr("href")
+		if(exists) {
+			result.PackageImageURL = a_href
+		}
+	})
+
+	doc.Find("table.mg-b20").First().Find("a[href *= 'article=actor']").Each(func(index int, selection *goquery.Selection) {
+		actress := Actress{}
+		actress.Name = selection.Text()
+
+		href, exists := selection.Attr("href")
+		if(exists) {
+			actress.Id = href
+		}
+
+		result.ActressList = append(result.ActressList, &actress)
+	})
 
 	return &result
 }
